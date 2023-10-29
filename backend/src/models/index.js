627878dd4136e481a46260d6a6bb4a43b6a3df1a
MdGazanfar <gazanfaransari6@gmail.com>
2023-10-29 18:51:47 +0530

    deleted

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     
     const dbName = 'lectureSchedule';
-    const url = `mongodb://0.0.0.0:27017/${dbName}`
-
+    // const url = `mongodb://0.0.0.0:27017/${dbName}`
+    const url = `mongodb+srv://ansarigazanfar679:fO5BPkFGwFaeqEGu@cluster0.81yrwgd.mongodb.net/?retryWrites=true&w=majority/${dbName}`
+    
     try {
         await mongoose.connect(url, {
             useNewUrlParser: true,
